7cca0db5ffe2ed8ff40b79be9c0e512ca3b576c1
Victor <victor5055@outlook.com>
2022-11-16 15:49:40 -0500

    Adding POST AND GET API REQUEST.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,33 @@
-const express = require("express");
-const router = express.Router();
+const app = require('express').Router();
+const fs = require('fs');
+let db = require('../db/db.json');
 
-const add = require("../db/add");
+//Get API Request
+app.get('/notes', (req, res) => {
+    db = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
+  
+    res.json(db);
+  
+  });
+  
+  //POST API Request
+  app.post('/notes', (req, res) => {
+    let newNote = { 
+      id: Math.floor(Math.random() * 1000),
+      title: req.body.title,
+      text: req.body.text
+    }
+  
+    //A Form Of A Controller
+    db.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(db), (err, res) => {
+      if(err) throw err;
+    });
+  
+    res.json(db);
+  
+  })
+  
+  
+  //Exporting app//
+  module.exports = app;
