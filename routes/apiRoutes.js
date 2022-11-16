3957cd914eca4a0c0613f0bfa5e4fceb78f2bf26
Victor <victor5055@outlook.com>
2022-11-16 16:47:30 -0500

    Finishing the routes and adding Heroku.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -28,6 +28,31 @@ app.get('/notes', (req, res) => {
   
   })
   
+    // Adding Delete
+    app.delete('/notes/:id', (req, res) => {
+        let db = require('../db/db.json');
+        
+        let notesToKeep = [];
+      
+        for(let i = 0; i < db.length; i++) {
+          console.log(req.params.id)
+          console.log(db[i].id)
+      
+          if (parseInt(db[i].id) !== parseInt(req.params.id)) {
+            notesToKeep.push(db[i]);
+          }
+        }
+      
+        console.log(notesToKeep);
+      
+        db = notesToKeep;
+        fs.writeFileSync('./db/db.json', JSON.stringify(db), (err, res) => {
+          if(err) throw err;
+        });
+      
+        res.json(db);
+      
+      });
   
-  //Exporting app//
+      //Exporting app//
   module.exports = app;
