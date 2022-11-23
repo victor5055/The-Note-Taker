5d0340d034a2e6f355a599cab1a27ec14b2d0b9a
Victor <victor5055@outlook.com>
2022-11-23 15:27:49 -0500

    Adding the html routes.

--- server.js
+++ server.js
@@ -11,3 +11,89 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// HTML Routes
+
+
+//  Route that sends the user first to the AJAX Page
+// Acuring the index.html
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+// Acuring the notes.html
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+
+});
+// Posting the notes
+app.post("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+    if (error) {
+      return console.log(error)
+    }
+    notes = JSON.parse(notes)
+
+    const id = notes[notes.length - 1].id + 1
+    const newNote = { title: req.body.title, text: req.body.text, id: id }
+    const activeNote = notes.concat(newNote)
+
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(activeNote), function (error, data) {
+      if (error) {
+        return error
+      }
+      console.log(activeNote)
+      res.json(activeNote);
+    })
+  })
+})
+
+// Pulling from db.json
+app.get("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, data) {
+    if (error) {
+      return console.log(error)
+    }
+    console.log("This is Notes", data)
+    res.json(JSON.parse(data))
+  })
+});
+//Deleting the notes
+app.delete("/api/notes/:id", function (req, res) {
+  const noteId = JSON.parse(req.params.id)
+  console.log(noteId)
+  fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+    if (error) {
+      return console.log(error)
+    }
+    notes = JSON.parse(notes)
+
+    notes = notes.filter(val => val.id !== noteId)
+
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (error, data) {
+      if (error) {
+        return error
+      }
+      res.json(notes)
+    })
+  })
+})
+// routes the HTTP PUT requests to the specified path 
+app.put("/api/notes/:id", function(req, res) {
+  const noteId = JSON.parse(req.params.id)
+  console.log(noteId)
+  fs.readFile(__dirname + "db/db.json", "utf8", function(error, notes) {
+    if (error ){
+      return console.log(error)
+    }
+    notes.JSONparse(notes)
+
+    notes = notes.filter(val => val.id !== noteId)
+
+    fs.writeFile(__dirname +"db/db.json", JSON.stringify(notes), function (error, data) {
+      if (error) {
+        return error
+      }
+      res.json(notes)
+    })
+  })
+})
